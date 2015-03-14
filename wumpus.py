bd39408fc689cbaf382bc0b7f18d868de31031d1
rvvermeulen <rvvermeulen@hotmail.nl>
2015-03-14 15:40:27 +0100

    Startscreen, endscreen and navigation added

--- wumpus.py
+++ wumpus.py
@@ -5,7 +5,7 @@
 import sys
 from random import randrange
 from collections import namedtuple
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
 classic_5x4 = ["es","esw","esw","esw","sw","nes","nesw","nesw","nesw","nsw","nes","nesw","nesw","nesw","nsw","ne","new","new","new","nw"]
 black_5x4 = ["black","black","black","black","black","black","black","black","black","black","black","black","black","black","black","black","black","black","black","black",]
@@ -16,6 +16,161 @@ black_5x4 = ["black","black","black","black","black","black","black","black","bl
 #   16, 17, 18, 19, 20
 
 XY = namedtuple("XY", "x, y")
+
+class Window(QtGui.QWidget):
+    """Generates main window and navigates between screens"""
+
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.startscreen = StartScreen(self)
+        self.setGeometry(400,200,700,400)
+        self.setWindowTitle("Hunt the Wumpus")
+
+        self.show()
+
+    def toStartScreen(self):
+        self.startscreen = StartScreen(self)
+        self.startscreen.show()
+
+    def toGameScreen(self, level):
+        self.gamescreen = GameScreen(self, level)
+        self.gamescreen.show()
+
+    def toEndScreen(self, result, gold):
+        self.endscreen = EndScreen(self, result, gold)
+        self.endscreen.show()
+
+#============================================================================================
+
+class StartScreen(QtGui.QWidget):
+    """Generates start screen"""
+
+    def __init__(self, parent):
+        super(StartScreen, self).__init__(parent)
+        self.parent = parent
+
+        self.classic_button = QtGui.QPushButton("Classic", self)
+        self.classic_button.clicked.connect(self.chooseLevel)
+
+    def chooseLevel(self):
+        self.parent.toGameScreen(classic_5x4)
+        self.close()
+
+class GameScreen(QtGui.QWidget):
+    """Generates game screen"""
+
+    def __init__(self, parent, level):
+        super(GameScreen, self).__init__(parent)
+        self.parent = parent
+
+        self.sidebar = SideBar(self)
+        self.gamefield = GameField(self, level)
+
+    def quitGame(self):
+        self.parent.toStartScreen()
+        self.close()
+
+class EndScreen(QtGui.QWidget):
+    """Generates end screen"""
+
+    def __init__(self, parent, result, gold):
+        super(EndScreen, self).__init__(parent)
+        self.parent = parent
+
+        text_label = QtGui.QLabel(self)
+        gold_label = QtGui.QLabel(self)
+        main_button = QtGui.QPushButton("Main", self)
+
+        text_label.move(250,150)
+        gold_label.move(270,230)
+
+        if result == "win":
+            text_label.setText("Many who entered these caves,\nfell pray to the Wumpus and were never seen again...\nUntil you heroically hunted him down and killed the feared beast!\nCongratulations! You killed the Wumpus!")
+        else:
+            text_label.setText("Many who entered these caves,\nfell pray to the Wumpus and were never seen again...\nAnd you blindly followed them in their fate.\nYou died, and the Wumpus will continue to terrorize these caves!")
+
+        gold_label.setText("Gold: " + str(gold))
+
+        main_button.clicked.connect(self.newGame)
+        
+    def newGame(self):
+        self.parent.toStartScreen()
+        self.close()
+        
+#============================================================================================
+
+class SideBar(QtGui.QWidget):
+    """Creates left side bar in game screen"""
+
+    def __init__(self, parent):
+        super(SideBar, self).__init__(parent)
+        self.parent = parent
+
+        self.setGeometry(0,0,200,400)
+        self.quitbutton = QtGui.QPushButton("Quit", self)
+        self.quitbutton.clicked.connect(self.parent.quitGame)
+
+class GameField(QtGui.QWidget):
+    """Creates the game field with a list of tile names predetermined by the programmer."""
+
+    def __init__(self, parent, level):
+        super(GameField, self).__init__(parent)
+
+        self.initUI(level)
+        self.placeItemsRandomly()
+
+    def initUI(self, level):
+        self.width = 5
+        self.height = 4
+        self.seize = self.width*self.height
+        self.field = level
+
+        self.setGeometry(200,0,self.width*100,self.height*100)
+        
+        positions = []
+        for y in range(self.height):
+            for x in range(self.width):
+                positions.append(XY(x,y)) 
+        
+        self.tile_dic = {}
+        for i in range(self.seize):
+            self.tile_dic[positions[i]] = Tile(self, self.field[i], positions[i])
+            
+        for key in self.tile_dic:
+            o = self.tile_dic[key]
+            o.setGeometry(o.position.x*100, o.position.y*100, 100, 100)
+            o.findCenter()
+
+        print(self.tile_dic[(4,3)].center)#Test function, this is how you get the center of a 'coordinate'
+        print(self.tile_dic[(4,3)].W_open)#Test function, this is how you can see if that direction is open for Jack and Wumpus (True) or if there's a wall (False)
+        
+    def placeItemsRandomly(self):
+        new_gold_amount = 5
+        new_hole_amount = 1
+        new_bats_amount = 1
+        
+        positions_list = []
+        
+        while len(positions_list) < new_gold_amount+new_hole_amount+new_bats_amount:
+                x = randrange(self.width)
+                y = randrange(self.height)
+                
+                if not ((x,y) in positions_list):
+                        positions_list.append((x,y))
+        
+        for position in positions_list:
+                if new_gold_amount > 0:
+                        self.tile_dic[position].setItem("gold")
+                        new_gold_amount = new_gold_amount - 1
+                elif new_hole_amount > 0:
+                        self.tile_dic[position].setItem("hole")
+                        new_hole_amount = new_hole_amount - 1
+                elif new_bats_amount > 0:
+                        self.tile_dic[position].setItem("bats")
+                        new_bats_amount = new_bats_amount - 1
+        
+#============================================================================================
 
 class Tile(QtGui.QWidget):
     """Creates a field tile and all of its atributes."""
@@ -31,24 +186,24 @@ class Tile(QtGui.QWidget):
         self.setType(name)
         
     def setItem(self, item):#item is: bats, hole, gold
-		if item == "bats":
-			self.bats = True
-		elif item == "gold":
-			self.gold = True
-		elif item == "hole":
-			self.hole = True
-		else:
-			pass
+        if item == "bats":
+                self.bats = True
+        elif item == "gold":
+                self.gold = True
+        elif item == "hole":
+                self.hole = True
+        else:
+                pass
 		
     def removeItem(self, item):#item is: bats, hole, gold
-		if item == "bats":
-			self.bats = False
-		elif item == "gold":
-			self.gold = False
-		elif item == "hole":
-			self.hole = False
-		else:
-			pass
+        if item == "bats":
+                self.bats = False
+        elif item == "gold":
+                self.gold = False
+        elif item == "hole":
+                self.hole = False
+        else:
+                pass
 
     def setImage(self, image_name):
         """Sets and show image of tile"""
@@ -158,73 +313,13 @@ class Tile(QtGui.QWidget):
             self.E_open = True
             self.S_open = True
             self.W_open = True
-
-class GameField(QtGui.QWidget):
-    """Creates the game field with a list of tile names predetermined by the programmer."""
-
-    def __init__(self):
-        super(GameField, self).__init__()
-
-        self.initUI()
-        self.placeItemsRandomly()
-
-    def initUI(self):
-        self.width = 5
-        self.height = 4
-        self.seize = self.width*self.height
-        self.field = classic_5x4
-
-        self.setGeometry(400,200,self.width*100,self.height*100)
         
-        positions = []
-        for y in range(self.height):
-            for x in range(self.width):
-                positions.append(XY(x,y)) 
-        
-        self.tile_dic = {}
-        for i in range(self.seize):
-            self.tile_dic[positions[i]] = Tile(self, self.field[i], positions[i])
-            
-        for key in self.tile_dic:
-            o = self.tile_dic[key]
-            o.setGeometry(o.position.x*100, o.position.y*100, 100, 100)
-            o.findCenter()
-
-        print(self.tile_dic[(4,3)].center)#Test function, this is how you get the center of a 'coordinate'
-        print(self.tile_dic[(4,3)].W_open)#Test function, this is how you can see if that direction is open for Jack and Wumpus (True) or if there's a wall (False)
-        
-    def placeItemsRandomly(self):
-		new_gold_amount = 5
-		new_hole_amount = 1
-		new_bats_amount = 1
-		
-		positions_list = []
-		
-		while len(positions_list) < new_gold_amount+new_hole_amount+new_bats_amount:
-			x = randrange(self.width)
-			y = randrange(self.height)
-			
-			if not ((x,y) in positions_list):
-				positions_list.append((x,y))
-		
-		for position in positions_list:
-			if new_gold_amount > 0:
-				self.tile_dic[position].setItem("gold")
-				new_gold_amount = new_gold_amount - 1
-			elif new_hole_amount > 0:
-				self.tile_dic[position].setItem("hole")
-				new_hole_amount = new_hole_amount - 1
-			elif new_bats_amount > 0:
-				self.tile_dic[position].setItem("bats")
-				new_bats_amount = new_bats_amount - 1
-			
-			
+#============================================================================================
 
 def main():
     
     app = QtGui.QApplication(sys.argv)
-    window = GameField()
-    window.show()
+    window = Window()
     sys.exit(app.exec_())
     
 
