7d363300c0c1a246201624449209e3d0e582bacc
Eyokiha <h.veldthuis@gmail.com>
2015-03-21 14:28:01 +0100

    pijl kan worden afgevuurd en draait mee met richting

--- mc.py
+++ mc.py
@@ -3,8 +3,6 @@
 import sys
 import random
 import time
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
 from PyQt4 import QtCore, QtGui
 
 class McView (QtGui.QGraphicsView):
@@ -19,9 +17,9 @@ class Mc (QtGui.QWidget):
     def __init__(self, parent, initial_mc_position):
         
         super(Mc, self).__init__(parent)
-        self.parent = parent
+        self.parent      = parent
         self.mc_position = initial_mc_position
-        self.mc_coords = [self.mc_position.x*100+49, self.mc_position.y*100+49]
+        self.mc_coords   = [self.mc_position.x*100+49, self.mc_position.y*100+49]
         self.initUI()
         
     def initUI(self):
@@ -35,9 +33,11 @@ class Mc (QtGui.QWidget):
     def controlMc(self):
         """ Assign mc attributes. """
         
-        self.mc_walking    = False
-        self.tile_width    = 100
-        self.space_pressed = False
+        self.mc_walking        = False
+        self.arrow_shooting    = False
+        self.times_arrow_moved = 0
+        self.tile_width        = 100
+        self.space_pressed     = False
 
         self.pixmap   = QtGui.QPixmap("mc_ph.png")
         self.mc_view  = McView(self)
@@ -47,22 +47,25 @@ class Mc (QtGui.QWidget):
 
         self.mc_char  = self.mc_scene.addPixmap(self.pixmap)
         
-        self.moveMc()
+        self.mc_char.setPos(self.mc_coords[0], self.mc_coords[1])
         
     def keyPressEvent(self, e):
         """ Define actions for all key press events. """
 
-        if self.mc_walking == False: # no input allowed while mc is walking
+        if self.mc_walking == False and self.arrow_shooting == False: # no input allowed while mc is walking
             #--- left arrow --#
             if e.key() == QtCore.Qt.Key_Left:
                 print("--Left Arrow")
                 
                 if self.space_pressed == False and self.parent.tile_dic[self.mc_position].W_open:
+                    # move player position
                     self.mc_walking = True
                     self.animateMc(0, -1)
                     self.parent.parent.endTurn()
                 else:
                     # move arrow position
+                    self.arrow_shooting = True
+                    self.moveArrow("left")
                     pass
                 
                 print(self.mc_coords)
@@ -72,11 +75,14 @@ class Mc (QtGui.QWidget):
                 print("--Right Arrow")
                 
                 if self.space_pressed == False and self.parent.tile_dic[self.mc_position].E_open:
+                    # move player position
                     self.mc_walking = True
                     self.animateMc(0, 1)
                     self.parent.parent.endTurn()
                 else:
                     # move arrow position
+                    self.arrow_shooting = True
+                    self.moveArrow("right")
                     pass
             
                 print(self.mc_coords)
@@ -86,11 +92,14 @@ class Mc (QtGui.QWidget):
                 print("--Up Arrow")
             
                 if self.space_pressed == False and self.parent.tile_dic[self.mc_position].N_open:
+                    # move player position
                     self.mc_walking = True
                     self.animateMc(1, -1)
                     self.parent.parent.endTurn()
                 else:
                     # move arrow position
+                    self.arrow_shooting = True
+                    self.moveArrow("up")
                     pass
             
                 print(self.mc_coords)
@@ -100,11 +109,14 @@ class Mc (QtGui.QWidget):
                 print("--Down Arrow")
                 
                 if self.space_pressed == False and self.parent.tile_dic[self.mc_position].S_open:
+                    # move player position
                     self.mc_walking = True
                     self.animateMc(1, 1)
                     self.parent.parent.endTurn()
                 else:
                     # move arrow position
+                    self.arrow_shooting = True
+                    self.moveArrow("down")
                     pass
                 
                 print(self.mc_coords)
@@ -115,9 +127,11 @@ class Mc (QtGui.QWidget):
                 
                 if self.space_pressed == True:
                     self.space_pressed = False
+                    self.initializeArrow(False) # remove arrow
                     print("Arrow canceled")
                 else:
                     self.space_pressed = True
+                    self.initializeArrow(True) # create arrow
                     print("Arrow ready")
                 
                 print(self.space_pressed == True)
@@ -130,28 +144,63 @@ class Mc (QtGui.QWidget):
                     print("Arrow shot")
                     self.parent.parent.endTurn()
             
-        else:
-            pass
 
-    def animateMc(self, l_or_r, u_or_d):
+    def animateMc(self, x_or_y, direction):
         self.stepcounter = 0
         while self.mc_walking == True:
-            time.sleep(0.02)
-            self.mc_coords[l_or_r] += u_or_d
+            time.sleep(0.01)
+            self.mc_coords[x_or_y] += direction
             self.stepcounter += 1
-            self.mc_char.setOffset(self.mc_coords[0], self.mc_coords[1])
-            #self.moveMc()
-            QtGui.QApplication.processEvents()
-            if self.stepcounter == self.tile_width:
+            self.mc_char.setPos(self.mc_coords[0], self.mc_coords[1]) # change mc position to new coordinates
+            QtGui.QApplication.processEvents() # needed to update gui, normally won"t update during loop
+            if self.stepcounter == self.tile_width: # stop loop when the mc has traveled the length of one tile
                 self.mc_walking = False
-    
+
+    def animateArrow(self, x_or_y, direction):
+        self.stepcounter = 0        
+        while self.arrow_shooting == True:    
+            time.sleep(0.01)
+            self.arrow_coords[x_or_y] += direction
+            self.stepcounter += 1
+            self.arrow.setPos(self.arrow_coords[0], self.arrow_coords[1]) # change arrow position to new coordinates
+            QtGui.QApplication.processEvents() # needed to update gui, normally won"t update during loop
+            if self.stepcounter == self.tile_width: # stop loop when the arrow has traveled the length of one tile
+                self.arrow_shooting = False
+                
+    def initializeArrow(self, create_arrow):
+        self.times_arrow_moved = 0
+        if create_arrow == True:
+            self.arrow_pixmap = QtGui.QPixmap("images/arrow.png")
+            self.arrow  = self.mc_scene.addPixmap(self.arrow_pixmap)
+            self.arrow.setTransformOriginPoint(self.arrow_pixmap.width() / 2, self.arrow_pixmap.height() / 2)
+            self.arrow_coords = self.mc_coords[:] # copy mc coordinates
+            self.arrow.setPos(self.arrow_coords[0], self.arrow_coords[1])
+        else:
+            # needs to remove arrow
+            self.mc_scene.removeItem(self.arrow)
+            pass
+        
+    def moveArrow(self, key):        
+        if self.times_arrow_moved >= 5:
+            self.arrow_shooting = False
+            print("Arrow too far, can't move any further")
+        else:
+            self.times_arrow_moved += 1
+            if key == "left":
+                self.arrow.setRotation(180)
+                self.animateArrow(0, -1)
+            if key == "right":
+                self.arrow.setRotation(0)
+                self.animateArrow(0, 1)
+            if key == "up":
+                self.arrow.setRotation(270)
+                self.animateArrow(1, -1)
+            if key == "down":
+                self.arrow.setRotation(90)
+                self.animateArrow(1, 1)
+
     def updateMcPosition(self):
         self.mc_position = ((self.mc_coords[0]-49)/100, (self.mc_coords[1]-49)/100)
-        
-    def moveMc(self):
-        print('moving')
-        self.mc_char.setOffset(self.mc_coords[0], self.mc_coords[1])
-        #self.mc_view.centerOn(self.mc_coords[0], self.mc_coords[1])
             
 
 
